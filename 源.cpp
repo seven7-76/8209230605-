62b9b8f9ca7a8fba8df6fb97377bc11471853a0b
东北第一深情 <149564695+seven7-76@users.noreply.github.com>
2023-11-25 16:00:52 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,13 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-	int k = 1;
-	cout << k++ << endl;
-	int i = 1;
-	cout << i++ << endl;
-	cout << "Welcome to C++" << endl;
-	return 0;
-}
 
-	
+int main() {
+    double price = 0.8; 
+    int num = 2; 
+    int totalNum = num; 
+    double totalCost = num * price; 
+    int days = 1; 
+
+    while (num <= 100) {
+        num *= 2; 
+        totalNum += num; 
+        totalCost += num * price; 
+        days++;
+    }
+
+    double averageCost = totalCost / days; 
+    cout << "每天平均花费：" << averageCost << "元" << endl;
+
+    return 0;
+}
